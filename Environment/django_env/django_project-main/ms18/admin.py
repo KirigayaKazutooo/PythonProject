06b75eb089e90c4a3e53f1cf546804c58fec4394
KirigayaKazutooo <frimegwapo@gmail.com>
2024-01-10 04:31:14 +0800

    first commit

--- Environment/django_env/django_project-main/ms18/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Product, PurchaseOrder
-
-admin.site.register(Product)
-admin.site.register(PurchaseOrder)
--- Environment/django_env/django_project-main/ms18/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Ms18Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ms18'
